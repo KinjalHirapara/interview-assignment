34670822d4b73b2f22d83e49011b9cd8a3547aab
kinjalradadiya <kinjal01radadiya@gmail.com>
2023-06-08 19:17:12 +0530

    Applied validation in login form.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors  } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+  myForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.myForm = this.formBuilder.group({
+      userName: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(8), this.passwordValidator]]
+    });
+  }
+
+  passwordValidator(control: AbstractControl): ValidationErrors | null {
+    const value: string = control.value;
+    const hasNumber = /[0-9]/.test(value);
+    const hasSymbol = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/.test(value);
+    const hasCapitalLetter = /[A-Z]/.test(value);
+    const hasSmallLetter = /[a-z]/.test(value);
+
+    const valid = hasNumber && hasSymbol && hasCapitalLetter && hasSmallLetter;
+    console.log(valid);
+    return valid ? null : { invalidPassword: true };
+  }
+
+  onSubmit() {
+    if (this.myForm.valid) {
+      // Form is valid, perform necessary actions
+      console.log(this.myForm.value);
+    }
+  }
 
 }
